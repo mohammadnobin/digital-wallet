95962a4120636b2f8c92003c1296bf38df5f84f0
Fahad-smto <simanto.smto.prog@gmail.com>
2025-09-15 17:53:48 +0600

    new branch has created

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <h1 className="text-4xl font-bold">Welcome to My Next.js App!</h1>
+    <p className="mt-4 text-lg">This is a simple starter template.</p>
     </main>
   );
 }
