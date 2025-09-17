29cd5b4d5dd3db44ee05c04aedba125e6b43aa86
Reazul Islam Reaz <reazulislam1487@gmail.com>
2025-09-17 23:27:59 +0600

    added

--- src/app/components/shared/Hero.jsx
+++ src/app/components/shared/Hero.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      Hero
+      <div>
+        <h1>crate by Nobin</h1>
+        <h2>Create by Reaz</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
