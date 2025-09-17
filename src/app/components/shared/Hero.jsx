acc73456665b05d1074839a01f1d532f8da017a2
Md.Nobin <mdnobin.fdr@gmail.com>
2025-09-17 13:07:35 +0600

    added some in hero

---
+++ src/app/components/shared/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return
+  <>
+   <div>Hero</div>;
+   <div>Hero</div>;
+   <div>Hero</div>;
+   <div>Hero</div>;
+  </>
+};
+
+export default Hero;
