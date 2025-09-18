a4c522a5666e1e48d7945d1dd0de94d4a0c81292
Md.Nobin <mdnobin.fdr@gmail.com>
2025-09-19 00:40:15 +0600

    making a beautifull wroksection

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import FeaturesSection from "./components/Home/Features";
 import HeroSection from "./components/Home/Hero";
+import HowItWorksSection from "./components/Home/worksSection";
 
 export default function Home() {
   return (
      <div>
       <HeroSection />
       <FeaturesSection />
+      <HowItWorksSection />
      </div>
   );
 }
