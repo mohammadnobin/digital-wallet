2055f91303879395be0d490edd45cf2768108d82
Md.Nobin <mdnobin.fdr@gmail.com>
2025-09-19 00:34:34 +0600

    make features seciton

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import HeroSection from "./components/shared/Hero";
+import FeaturesSection from "./components/Home/Features";
+import HeroSection from "./components/Home/Hero";
 
 export default function Home() {
   return (
      <div>
       <HeroSection />
+      <FeaturesSection />
      </div>
   );
 }
