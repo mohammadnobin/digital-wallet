e72f076e7fdb9273a649a65dda0ff23725711f7a
shehab99b <shehabulislam429@gmail.com>
2025-09-20 00:48:13 +0600

    added faq sections

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -135,7 +135,7 @@ const page = () => {
                             </button>
                             <button className="bg-gradient-to-r from-pink-500 to-purple-500 text-white px-8 py-4 rounded-full font-semibold text-lg hover:from-pink-400 hover:to-purple-400 transition-all duration-300 hover:scale-105 hover:shadow-lg flex items-center justify-center gap-2">
                                 <Phone className="w-5 h-5" />
-                                Call Us
+                                Contract Us
                             </button>
                         </div>
                     </div>
