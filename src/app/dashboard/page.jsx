fa9027dedcb9edd62183a8cd405755e7ec217501
jannatulnila <jannatulnila74@gmail.com>
2025-09-24 23:58:47 +0600

    added some code

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Navbar from '../components/shared/Navbar';
 
-export default function page(props) {
+export default function page() {
     return (
-        <div>
-            <Navbar></Navbar>
+        <div> 
             
         </div>
     );
