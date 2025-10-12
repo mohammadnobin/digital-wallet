052745ea2a37b58e44cfaa44d20f0759d5ee05d1
jannatulnila <jannatulnila74@gmail.com>
2025-10-13 01:32:52 +0600

    remove some code

--- src/app/page.js
+++ src/app/page.js
@@ -9,12 +9,9 @@ import WalletSlider from "./components/Home/AboutUs";
 
 import FooterSection from "./components/shared/Footer";
 import AboutUs from "./components/Home/AboutUs";
-<<<<<<< HEAD
 import FaqSection from "./components/Home/FaqSection";
-=======
 import WhyTrustUs from "./components/Home/WhyTrustUs";
 import StatsSection from "./components/Home/State";
->>>>>>> 7faa9328af3f092f1f3cb0b618b519b14e87aafb
 
 export default async function  Home() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users`)
@@ -24,21 +21,13 @@ export default async function  Home() {
     <Navbar />
       <HeroSection />
       <AboutUs />
-<<<<<<< HEAD
-      {/* <BankHero /> */}
-      {/* <State /> */}
       <AppPreview />
       <Features/>
-      {/* <TestimonialsSection /> */}
       <FaqSection></FaqSection>
-=======
-      <BankHero />
-      <AppPreview />
       <Features/>
       <WhyTrustUs />
       <StatsSection />
       <TestimonialsSection />
->>>>>>> 7faa9328af3f092f1f3cb0b618b519b14e87aafb
       <FooterSection></FooterSection>
     </>
   );
