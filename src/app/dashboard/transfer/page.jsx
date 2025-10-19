4e543f90a4b6df8a361dc776f5b932cc230e5265
Md.Nobin <mdnobin.fdr@gmail.com>
2025-10-20 00:53:02 +0600

    Implement money transfer functionality with user authentication and transfer history display

--- src/app/dashboard/transfer/page.jsx
+++ src/app/dashboard/transfer/page.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
 import MoneyTransfer from '../components/transfer';
+import authOptions from "@/lib/authOptions";
+import axios from "axios";
+import { getServerSession } from 'next-auth/next';
 
-const page = () => {
+const page = async () => {
+    const session = await getServerSession(authOptions);
+  if (!session) {
+    return <div>Please login to access this page</div>;
+  }
+  const email = session.user.email;
+  const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/email?email=${encodeURIComponent(email)}`);
+  const user = res.data;
     return (
         <div>
-          <MoneyTransfer />
+          <MoneyTransfer user={user} />
         </div>
     );
 };
